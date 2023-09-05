e818e97623778f283559c541d88569e214ff81e9
Savindu Shehan <savindushehansamaraweera@gmail.com>
2023-09-05 21:39:29 +0530

    add javascript part

---
+++ JS/javascript.js
@@ -0,0 +1,49 @@
+
+
+var Z1=document.getElementById("z1");
+var Z2=document.getElementById("z2");
+var Z3=document.getElementById("z3");
+var Z4=document.getElementById("z4");
+var Z5=document.getElementById("z5");
+var M1=document.getElementById("m1");
+var M2=document.getElementById("m2");
+var M3=document.getElementById("m3");
+var M4=document.getElementById("m4");
+var M5=document.getElementById("m5");
+
+
+
+M1.onmousemove=function(){
+    Z1.style.display="block";
+}
+M1.onmouseout=function(){
+    Z1.style.display="none";
+}
+
+M2.onmousemove=function(){
+    Z2.style.display="block";
+}
+M2.onmouseout=function(){
+    Z2.style.display="none";
+}
+
+M3.onmousemove=function(){
+    Z3.style.display="block";
+}
+M3.onmouseout=function(){
+    Z3.style.display="none";
+}
+
+M4.onmousemove=function(){
+    Z4.style.display="block";
+}
+M4.onmouseout=function(){
+    Z4.style.display="none";
+}
+
+M5.onmousemove=function(){
+    Z5.style.display="block";
+}
+M5.onmouseout=function(){
+    Z5.style.display="none";
+}
