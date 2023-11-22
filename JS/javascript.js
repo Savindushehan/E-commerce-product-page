aa08d836843720e1c5e7f78eb3b4e9a072f226c4
Savindu Shehan <savindushehansamaraweera@gmail.com>
2023-11-22 12:10:27 +0530

    create some fetures

--- JS/javascript.js
+++ JS/javascript.js
@@ -40,7 +40,7 @@ var P19=document.getElementById("p19");
 var Body=document.getElementById("body");
 var F1=document.getElementById("f1");
 var FOOTER=document.getElementById("footer");
-
+var CCC=document.getElementById("ccc");
 
 
 
@@ -111,6 +111,8 @@ BTN.onclick=function(){
     CART.style.display="block";
     CART2.innerHTML="$125.00 x "+y+" = "+(125.0*y);
     P5.style.filter="opacity(50%)";
+    CCC.style.display="block";
+    CCC.innerHTML=y;
     i2.style.border=" 4px solid hsl(26, 100%, 55%)";
 }
 P5.onclick=function(){
